e063363ddd964c5b9756be08ba374900cc9fe282
Jorge Eiras <j.eiras77@gmail.com>
2023-11-15 04:39:37 -0300

    'Se agrega React Router'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-//import Contacto from './components/Contacto';
+import Contacto from './components/Contacto';
 import Favoritos from './components/Favoritos';
 import Inicio from './components/Inicio';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Favoritos />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />}/>
+        <Route path="/contacto" element={<Contacto />}/>
+        <Route path="/favorito" element={<Favoritos />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
