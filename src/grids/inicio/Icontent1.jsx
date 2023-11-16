3d5079ef04585b052768b699e1954616f3c0e5b9
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-11-15 23:32:45 -0300

    NavBar del Inicio funcionando

--- src/grids/inicio/Icontent1.jsx
+++ src/grids/inicio/Icontent1.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Content1 = () => {
   return (
-    <div>
-      <img src="./imagenes/logo-misviajes.png" alt="Mis planes"/>
-      <div className="links">
+    <div className='navbar'>
+      <Link to="/"><img src="./imagenes/logo-misviajes.png" alt="Mis planes" /></Link>
+      
+      <ul class="nav nav-underline">
+        <li class="nav-item">
+          <Link to="/favorito" className="nav-link" id="navLink">Favoritos</Link>
+        </li>
+        <li class="nav-item">
+          <Link to="/contacto" className="nav-link" id="navLink">Contacto</Link>
+        </li>
+        <li class="nav-item">
+          <Link to="/login" className="btn btn-primary" id="navButton">Iniciar Sesión</Link>
+        </li>
+      </ul>
 
-      </div>
-
-    </div>
-        
+    </div>   
   )
 }
 
