f4c6e177e3bbc974c573bf5e4daaae8feec65b4c
FlorFuentes <flopy_200412@hotmail.com>
2023-11-16 17:46:08 -0300

    Footer contacto/login funcionando

--- src/grids/contacto/Content3.jsx
+++ src/grids/contacto/Content3.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Icontent4 from "../inicio/Icontent4"
 const Content3 = () => {
   return (
-    <footer>Footer</footer>
+    <footer><Icontent4/> </footer>
   )
 }
 
