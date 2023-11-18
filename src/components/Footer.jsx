31f6d1969d4bc89309a7a6426394ee520be7830b
Jorge Eiras <j.eiras77@gmail.com>
2023-11-18 05:56:56 -0300

    'LinkedIn Jorge Eiras'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/yaninamanh/" target="_blank" rel="noopener noreferrer">Yanina | </a>
         <a href="https://www.linkedin.com/in/elias-carrizo-5b477b165/" target="_blank" rel="noopener noreferrer">Elias | </a>
         <a href="falta" target="_blank" rel="noopener noreferrer">Matías | </a>
-        <a href="falta" target="_blank" rel="noopener noreferrer">Jorge | </a>
+        <a href="https://www.linkedin.com/in/jorge-eiras-7aa92b231" target="_blank" rel="noopener noreferrer">Jorge | </a>
         <a href="https://www.linkedin.com/in/luisgerardourtubey/" target="_blank" rel="noopener noreferrer">Luis</a>
 
       </div>
