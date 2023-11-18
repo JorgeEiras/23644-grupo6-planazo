228c171e61584bc0af235992fe941cd792030e22
Jorge Eiras <j.eiras77@gmail.com>
2023-11-18 05:50:54 -0300

    'Se termina el footer en todas las pantallas'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Content2 from '../grids/login/Lcontent2'
-import Content3 from '../grids/login/Lcontent3'
+
 import '../grids/login/Login.css'
 
 
@@ -14,9 +14,7 @@ const Login = () => {
                 <div className='lContent2 centered'>
                     <Content2/>
                 </div>
-                <div className='lContent3 centered text-white'>
-                    <Content3/>
-                </div>
+                
             </div>
         </section>
     </React.Fragment>
