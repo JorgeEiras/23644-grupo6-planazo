44dc8d73db4d48a5f72ab1309e0b2acd1d54fcbe
Jorge Eiras <j.eiras77@gmail.com>
2023-11-18 02:35:33 -0300

    'Se elimina Nabvar de Favoritos'

--- src/components/Favoritos.jsx
+++ src/components/Favoritos.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Content1 from '../grids/favoritos/Content1'
+
 import Content2 from '../grids/favoritos/Content2'
 import Content3 from '../grids/favoritos/Content3'
 import '../grids/favoritos/Favoritos.css'
@@ -10,9 +10,7 @@ const Favoritos = () => {
     <React.Fragment>
         <section>
             <div className='layoutFavoritos'>
-                <div className='content1 centered'>
-                    <Content1/>
-                </div>
+                
                 <div className='content2 centered'>
                     <Content2/>
                 </div>
