4bbd284d12c5717742f05ddb84d2d8158193dd48
Jorge Eiras <j.eiras77@gmail.com>
2023-11-20 23:46:36 -0300

    'Login con botones variables y definido'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,13 +6,32 @@ import '../grids/login/Login.css'
 
 
 const Login = () => {
+  const [isRegistrando, setIsRegistrando] = React.useState(false);
   return (
     <React.Fragment>
         <section>
             <div className='layoutLogin'>
                 
                 <div className='lContent2 centered'>
-                    <Content2/>
+                  <div>
+                      <h1> {isRegistrando ? "Regístrate" : "Inicia sesión"}</h1><br />
+                      <form >
+                        <label htmlFor="emailField"> Correo </label><br />
+                        <input type="email" id="emailField" /><br />
+                        <label htmlFor="passwordField"> Contraseña </label><br />
+                        <input type="password" id="passwordField" /><br /><br />
+                        <button type="submit">
+                          {" "}
+                          {isRegistrando ? "Regístrate" : "Inicia sesión"}{" "}
+                        </button>
+                      </form>
+                      <br /><br />
+                      <button onClick={() => setIsRegistrando(!isRegistrando)}>
+                        {isRegistrando
+                          ? "Iniciar sesión"
+                          : "Registrate gratis!"}
+                      </button>
+                  </div>
                 </div>
                 
             </div>
