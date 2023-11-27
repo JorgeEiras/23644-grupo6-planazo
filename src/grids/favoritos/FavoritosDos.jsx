676230579cdb90469af2fd158ab4d3bb01c2f21f
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-11-27 02:25:04 -0300

    Estaban trabajando con la api en la pagina de favoritos y no en la de inicio. Content 3 paso a ser Apilugares.jsx en grids/inicio

--- src/grids/favoritos/FavoritosDos.jsx
+++ src/grids/favoritos/FavoritosDos.jsx
@@ -10,47 +10,4 @@ const FavoritosDos = () => {
 
 export default FavoritosDos
 
-// import React, { useState, useEffect } from 'react'
-// import { useFetch } from "./useFetch";
-// import Travel from '../../components/Travel';
-// //import Content2 from './Content2';
-
-// const Content3 = ({ searchTerm }) => {
-
-//   const [url, setUrl] = useState("https://punctualturbodeletion--jeiras2020.repl.co/products/");
-
-//   useEffect(() => {
-//     if (searchTerm) {
-//       setUrl(`https://punctualturbodeletion--jeiras2020.repl.co/products/?search=${searchTerm}`);
-
-//     } else {
-//       setUrl("https://punctualturbodeletion--jeiras2020.repl.co/products/");
-//     }
-//     console.log("Soy content3", searchTerm);
-//   }, [searchTerm]);
-
-
-//   const { data, loading } = useFetch(url);
-
-
-//   return (
-//     <main>
-//       <div className='container-fluid'>
-//         <div className='row'>
-//           {loading && <li>Loading...</li>}
-//           {data?.map((travel) => (
-//             <div className='col-md-4' key={travel.post_id}>
-//               <Travel travel={travel} />
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </main>
-//   )
-// }
-
-// export default Content3
-
-//<li key={user.post_id}>{user.name}</li>
-
 
--- src/grids/inicio/Apilugares.jsx
+++ src/grids/inicio/Apilugares.jsx
@@ -1,10 +1,41 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useFetch } from "./useFetch";
+import Travel from '../../components/Travel';
 
-const Apilugares = () => {
+const Apilugares = ({ searchTerm }) => {
+
+  const [url, setUrl] = useState("https://punctualturbodeletion--jeiras2020.repl.co/products/");
+
+  useEffect(() => {
+    if (searchTerm) {
+      setUrl(`https://punctualturbodeletion--jeiras2020.repl.co/products/?search=${searchTerm}`);
+
+    } else {
+      setUrl("https://punctualturbodeletion--jeiras2020.repl.co/products/");
+    }
+    console.log("Soy content3", searchTerm);
+  }, [searchTerm]);
+
+
+  const { data, loading } = useFetch(url);
+  
+
   return (
     <main>
       <h1>Lugares API</h1>
+
+      <div className='container-fluid'>
+        <div className='row'>
+          {loading && <li>Loading...</li>}
+          {data?.map((travel) => (
+            <div className='col-md-4' key={travel.post_id}>
+              <Travel travel={travel} />
+            </div>
+          ))}
+        </div>
+      </div>
+    
     </main>
   )
 }
