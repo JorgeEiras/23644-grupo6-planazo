8c0b6666cfa1adfad5cd76d581fd6aea562a990c
lcassera <lilian.cassera@gmail.com>
2023-11-28 10:18:09 -0300

    Se agregó tiulo a favoritos

--- src/grids/favoritos/FavoritosUno.jsx
+++ src/grids/favoritos/FavoritosUno.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const FavoritosUno = () => {
   return (
-    <div className='favoritosUnoContainer'>
-      <h2>Favoritos 1</h2>
+    <div className='headFavorito'>
+      <h1>Mis favoritos</h1>
+      <button type="" className="btn btnAgregarFavoritos">Agregar favorito</button>
     </div>
+
+
+
+    
   )
 }
 
 export default FavoritosUno
+
+{/*<div className='favoritosUnoContainer'>
+      <h2>Favoritos 1</h2>
+  </div>*/}
