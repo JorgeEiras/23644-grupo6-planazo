58745e82b7bde3dc94219716fe1706f824f84fc5
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-11-28 16:29:44 -0300

    Estilo y corazoncito de la card funcionando

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 export default function Travel({ travel }) {
@@ -8,11 +8,23 @@ export default function Travel({ travel }) {
     objectFit: 'cover'
   }
 
+  const [showCorazonRojo, setShowCorazonRojo] = useState(false);
+
  
   return (
         
     <div className="card h-100" style={{ maxWidth: "20rem" }}>
-      <img className="card-img-top" src={travel.image.secure_url} alt={travel.name} style={imgStyle} />
+      <div>
+        <img className="card-img-top" src={travel.image.secure_url} alt={travel.name} style={imgStyle} />
+        <div style={{ position:"absolute", top:"0px", right:"0px"}}>
+          <div className="stage">
+            <div
+              className={showCorazonRojo ? "heart is-active" : "heart"}
+              onClick={() => setShowCorazonRojo(!showCorazonRojo)}>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="card-body mb-2">
         <h5 className="card-title">{travel.name}</h5>
         <p className="card-text text-truncate">{travel.description}</p>
