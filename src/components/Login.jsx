97be2e5cefb7c49256360481fef6479f219380f3
lcassera <lilian.cassera@gmail.com>
2023-11-29 13:49:28 -0300

    Ajustes en los mensajes de alerta mostrados durante el registro y el inicio de sesión

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,9 +6,6 @@ import { app } from "../fb";
 import '../grids/login/Login.css'
 
 import Swal from 'sweetalert2';
-/*import whithReactContent from 'sweetalert2-react-content';
-const MySwal = withReactContent(Swal);*/
-
 
 const Login = (props) => {
   //estado ..se está registrando o no
@@ -25,7 +22,7 @@ const Login = (props) => {
       })
       .catch((error) => {
         console.error("Error al registrar usuario:", error.message);
-        mostrarAlerta(error.message, "error");
+        mostrarMensajeError(error.message);       
       });
   };
 
@@ -33,10 +30,32 @@ const Login = (props) => {
     app
       .auth()
       .signInWithEmailAndPassword(correo, password)
-      .then((usuarioFirebase) => {
+      .then((usuarioFirebase) => {        
         console.log("sesión iniciada con:", usuarioFirebase.user);
         props.setUsuario(usuarioFirebase);
+        mostrarAlerta("Bienvenido " + usuarioFirebase.user.email, "success");/*Hay que corregir el dato del usuario que se muestra en el alert de bienvenida*/        
+      })
+      .catch((error) => {
+        console.error("Error al iniciar sesión:", error.message);
+        mostrarAlerta("Credenciales incorrectas. Verifica los datos ingresados", "error");
       });
+  };
+
+
+  const mostrarMensajeError = (mensaje) => {
+    let mensajePersonalizado = "Error desconocido"; // Mensaje predeterminado
+
+    if (mensaje.includes("auth/weak-password")) {
+      mensajePersonalizado = "La contraseña debe tener al menos 6 caracteres.";
+    } else if (mensaje.includes("auth/email-already-in-use")) {
+      mensajePersonalizado = "La dirección de correo electrónico ya está en uso.";
+    } else if (mensaje.includes("auth/invalid-email")) {
+      mensajePersonalizado = "La dirección de correo electrónico no es válida.";
+    }  
+
+    // Se pueden agregar más casos según sea necesario para otros posibles errores de Firebase
+
+    mostrarAlerta(mensajePersonalizado, "error");
   };
 
   const mostrarAlerta = (mensaje, tipo) => {
@@ -53,7 +72,7 @@ const Login = (props) => {
     e.preventDefault();
     const correo = e.target.emailField.value;
     const password = e.target.passwordField.value;
-    
+
     if (isRegistrando) {
       crearUsuario(correo, password);
     }
@@ -65,36 +84,36 @@ const Login = (props) => {
 
   return (
     <React.Fragment>
-        <section>
-            <div className='layoutLogin'>
-                
-                <div className='lContent2'>
-                  <div>
-                      <h1 className="tituloLogin"> {isRegistrando ? "Regístrate" : "Inicia sesión"}</h1>
-                      <h3 className="subTituloLogin">Ingresa con tu email y contraseña</h3>
-                      <form onSubmit={submitHandler} className="inicioSesion" >
-                        <label htmlFor="emailField"></label>
-                        <input type="email" id="emailField" placeholder="Email" /><br />
-                        <label htmlFor="passwordField"> </label>
-                        <input type="password" id="passwordField" placeholder="Contraseña"/><br />
-                        <button type="submit" className="btn btnIniciarSesion">
-                          {" "}
-                          {isRegistrando ? "Regístrate" : "Inicia sesión"}{" "}
-                        </button>
-               
-                      </form>
-                              <p className="textoRegistrate">No tengo cuenta.
-                      <button onClick={() => setIsRegistrando(!isRegistrando)} className="btn btnRegistrate">
-                        {isRegistrando
-                          ? "Iniciar sesión"
-                          : "Registrarme"}
-                      </button></p>  
-                  
-                  </div>
-                </div>
-                
+      <section>
+        <div className='layoutLogin'>
+
+          <div className='lContent2'>
+            <div>
+              <h1 className="tituloLogin"> {isRegistrando ? "Regístrate" : "Inicia sesión"}</h1>
+              <h3 className="subTituloLogin">Ingresa con tu email y contraseña</h3>
+              <form onSubmit={submitHandler} className="inicioSesion" >
+                <label htmlFor="emailField"></label>
+                <input type="email" id="emailField" placeholder="Email" /><br />
+                <label htmlFor="passwordField"> </label>
+                <input type="password" id="passwordField" placeholder="Contraseña" /><br />
+                <button type="submit" className="btn btnIniciarSesion">
+                  {" "}
+                  {isRegistrando ? "Regístrate" : "Inicia sesión"}{" "}
+                </button>
+
+              </form>
+              <p className="textoRegistrate">No tengo cuenta.
+                <button onClick={() => setIsRegistrando(!isRegistrando)} className="btn btnRegistrate">
+                  {isRegistrando
+                    ? "Iniciar sesión"
+                    : "Registrarme"}
+                </button></p>
+
             </div>
-        </section>
+          </div>
+
+        </div>
+      </section>
     </React.Fragment>
   )
 }
