2af8f5235d9b0f98579d73107b42bc5f60b7e1ca
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-12-01 01:04:41 -0300

    Separe el modal de Travel a otro componente TravelCardIndividual

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -13,6 +13,7 @@ export default function Travel() {
     objectFit: 'cover'
   }
 
+
   const [showCorazonRojo, setShowCorazonRojo] = useState(false); //variable para cambiar la clase del corazon
   const [favoritePlace, setFavoritePlace] = useState(''); // variable para obtener el nombre del lugar favorito
 
@@ -20,7 +21,8 @@ export default function Travel() {
     setShowCorazonRojo(!showCorazonRojo);
     setFavoritePlace(lugares.name);
   }
-  console.log({favoritePlace});
+  console.log({ favoritePlace });
+  
 
   return (
         
@@ -40,35 +42,12 @@ export default function Travel() {
       <div className="card-body mb-2">
         <h5 className="card-title">{ lugares.name }</h5>
         <p className="card-text text-truncate">{ lugares.description }</p>
-        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          <TravelCardIndividual
-            nombre={ lugares.name }
-            provincia={ lugares.province }
-            descripcion={ lugares.description }
-            ></TravelCardIndividual>
-          + Info
-        </button>
+        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+        >+ Info</button>
+        <TravelCardIndividual name={lugares.name} img={lugares.image.secure_url} province={lugares.province} description={lugares.description}
+        ></TravelCardIndividual>
       </div>
-
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">{lugares.name}</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body d-flex" style={{ flexDirection:'column', justifyContent: 'center' }}>
-              <h6>{lugares.province}</h6>
-              <img src={lugares.image.secure_url} alt={lugares.name} style={imgStyle} />
-              <p style={{ marginTop:'1rem' }}>{ lugares.description }</p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-              <button type="button" class="btn btn-primary">Agregar a favoritos</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </div>
    
   )
--- src/components/TravelCardIndividual.jsx
+++ src/components/TravelCardIndividual.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 
-export default function TravelCardIndividual() {
+export default function TravelCardIndividual({ name, img, province, description }) {
+
+    const imgStyle = {
+        height: '200px',
+        objectFit: 'cover'
+    }
 
   return (
       
-    <div class="modal modal-dialog modal-dialog-scrollable" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">{name}</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body d-flex" style={{ flexDirection: 'column', justifyContent: 'center' }}>
+                      <h6>{province}</h6>
+                      <img src={img} alt={name} style={imgStyle} />
+                      <p style={{ marginTop: '1rem' }}>{description}</p>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                      <button type="button" class="btn btn-primary">Agregar a favoritos</button>
+                  </div>
+              </div>
+          </div>
       </div>
       
   )
 }
+
+{/* <TravelCardIndividual
+    nombre={lugares.name}
+    provincia={lugares.province}
+    descripcion={lugares.description}
+></TravelCardIndividual> */}
