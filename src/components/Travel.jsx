e26f66d0f819adee562dbb22de9ab5efa2488753
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-12-02 04:07:53 -0300

    probando

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -8,28 +8,43 @@ export default function Travel() {
   //recupero la info de resultadosLugaresContext
   const lugares = useContext(resultadosLugaresContext);
 
+  //controla el corazon
+  const [showCorazonRojo, setShowCorazonRojo] = useState(false); //variable para cambiar la clase del corazon
+  const [favoritePlace, setFavoritePlace] = useState(''); // variable para obtener el nombre del lugar favorito
+
+  //controla el modal
+  const [showModal, setShowModal] = useState(false);
+  const [lugarID, setLugarId] = useState(0);
+
+
   const imgStyle = {
     height: '200px',
     objectFit: 'cover'
   }
 
 
-  const [showCorazonRojo, setShowCorazonRojo] = useState(false); //variable para cambiar la clase del corazon
-  const [favoritePlace, setFavoritePlace] = useState(''); // variable para obtener el nombre del lugar favorito
-
   const handleClick = () => {
     setShowCorazonRojo(!showCorazonRojo);
     setFavoritePlace(lugares.name);
   }
   console.log({ favoritePlace });
-  
+
+ 
+
+  const handleModal = () => {
+    setShowModal(true);
+    setLugarId(lugares.post_id);
+  }
+  console.log(lugarID);
+
+
 
   return (
-        
+
     <div className="card h-100" style={{ maxWidth: "20rem" }}>
       <div>
-        <img className="card-img-top" src={lugares.image.secure_url} alt={lugares.name} style={imgStyle}/>
-        <div style={{ position:"absolute", top:"0px", right:"0px"}}>
+        <img className="card-img-top" src={lugares.image.secure_url} alt={lugares.name} style={imgStyle} />
+        <div style={{ position: "absolute", top: "0px", right: "0px" }}>
           <div className="stage">
             <div
               className={showCorazonRojo ? "heart is-active" : "heart"}
@@ -40,15 +55,14 @@ export default function Travel() {
         </div>
       </div>
       <div className="card-body mb-2">
-        <h5 className="card-title">{ lugares.name }</h5>
-        <p className="card-text text-truncate">{ lugares.description }</p>
-        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+        <h5 className="card-title">{lugares.name}</h5>
+        <p className="card-text text-truncate">{lugares.description}</p>
+        <button onClick={handleModal} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
         >+ Info</button>
-        <TravelCardIndividual name={lugares.name} img={lugares.image.secure_url} province={lugares.province} description={lugares.description}
-        ></TravelCardIndividual>
+        <TravelCardIndividual showModal={showModal} lugarID={lugarID}></TravelCardIndividual>
       </div>
-      
+
     </div>
-   
+
   )
 }
--- src/components/TravelCardIndividual.jsx
+++ src/components/TravelCardIndividual.jsx
@@ -1,33 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useFetch } from '../grids/inicio/useFetch';
 
-export default function TravelCardIndividual({ name, img, province, description }) {
+export default function TravelCardIndividual({ showModal, lugarID }) {
+
+    const { data, loading } = useFetch("https://punctualturbodeletion--jeiras2020.repl.co/products/");
+    const [unLugar, setUnLugar] = useState({});
+    const [renderModal, setRenderModal] = useState(false);
 
     const imgStyle = {
         height: '200px',
         objectFit: 'cover'
     }
 
-  return (
-      
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-              <div class="modal-content">
-                  <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">{name}</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body d-flex" style={{ flexDirection: 'column', justifyContent: 'center' }}>
-                      <h6>{province}</h6>
-                      <img src={img} alt={name} style={imgStyle} />
-                      <p style={{ marginTop: '1rem' }}>{description}</p>
-                  </div>
-                  <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                      <button type="button" class="btn btn-primary">Agregar a favoritos</button>
-                  </div>
-              </div>
-          </div>
-      </div>
-      
-  )
+    const buscarUnLugar = (lugarID) => {
+        if (!loading) {
+            const lugarEncontrado = data.find(lugar => lugar.post_id === lugarID);
+            setUnLugar(lugarEncontrado || {})
+        }
+    };
+    console.log({ data });
+    console.log(unLugar);
+    
+
+    useEffect(() => {
+        if (showModal && lugarID > 0) {
+            buscarUnLugar(lugarID);
+            setRenderModal(true);
+        } else { 
+            console.log("modal nope")
+        }
+    },[showModal, lugarID, data ])
+
+    // useEffect(() => {
+    //     console.log(unLugar);
+    // }, [unLugar]); // hice esto para probar que el useeffect manda a renderizar cuando cambia unLugar pero
+    // //cuando hago click en "CUmbrecita" deja de actualizar, dios 
+    
+
+
+    return (
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                     <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">{ unLugar.name }</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body d-flex" style={{ flexDirection: 'column', justifyContent: 'center' }}>
+                        <h6>{unLugar.province}</h6>
+                        <img src={unLugar.name} alt={unLugar.name} style={imgStyle} />
+                        <p style={{ marginTop: '1rem' }}>{unLugar.description}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-primary">Agregar a favoritos</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
 }
